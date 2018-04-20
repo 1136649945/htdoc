6dc3740ac5da5b12ebb534bc91ea288e4523cd33
zhoupenghui@snsoft.com.cn <zhoupenghui@snsoft.com.cn>
2018-04-20 18:04:38 +0800

    新工程

---
+++ uibe/Application/Admin/Model/WordsModel.class.php
@@ -0,0 +1,19 @@
+<?php
+// +----------------------------------------------------------------------
+// | OneThink [ WE CAN DO IT JUST THINK IT ]
+// +----------------------------------------------------------------------
+// | Copyright (c) 2013 http://www.onethink.cn All rights reserved.
+// +----------------------------------------------------------------------
+// | Author: 麦当苗儿 <zuojiazi@vip.qq.com>
+// +----------------------------------------------------------------------
+
+namespace Admin\Model;
+use Think\Model;
+
+/**
+ * 导航模型
+ * @author 麦当苗儿 <zuojiazi@vip.qq.com>
+ */
+
+class WordsModel extends Model {
+}
