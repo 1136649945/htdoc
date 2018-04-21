6611272d474f36cc336f4797cf109056daa0d282
zhoupenghui@snsoft.com.cn <zhoupenghui@snsoft.com.cn>
2018-04-21 17:34:09 +0800

    word转html例子

--- agri/Application/Common/Controller/WordController.class.php
+++ agri/Application/Common/Controller/WordController.class.php
@@ -2,6 +2,7 @@
 namespace Common\Controller;
 
 use Think\Controller;
+use PhpOffice\PhpWord\IOFactory;
 /*
  * Zend/Escaper：https://github.com/zendframework/zend-escaper
 
@@ -12,15 +13,13 @@ Zend/Validator：https://github.com/zendframework/zend-validator
 class WordController extends Controller
 {
     //word转成html
-    public function word2html($docpath,$fileName)
+    public function word2html($docpath,$name)
     {
         import("Org.Util.PHPWord");
         if(is_file($docpath)){
-            $phpWord = new \PHPWord();
-            $phpWord->setDefaultFontName($docpath);
-//             $doc = IOFactory::load($docpath);
-//             $phpWord->setDocumentProperties($doc);
-            $phpWord->save($fileName,"HTML");
+            $phpWord = IOFactory::load($docpath);
+            $xmlWriter = IOFactory::createWriter($phpWord, "HTML");
+            $xmlWriter->save($name);
         }
     }
     
